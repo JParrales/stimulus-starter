057c8bc2c78df10d0ccdb071f7cb8ed10bd6c33f
Jose <josedparrales@gmail.com>
2021-09-27 22:36:50 -0500

    Designing For Resilience

--- src/controllers/clipboard_controller.js
+++ src/controllers/clipboard_controller.js
@@ -2,10 +2,17 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = [ "source" ]
+  static classes = [ "supported" ]
 
   copy() {
     event.preventDefault()
     this.sourceTarget.select()
     document.execCommand("copy")
+  }
+
+  connect() {
+    if (document.queryCommandSupported("copy")) {
+      this.element.classList.add(this.supportedClass)
+    }
   }
 }
